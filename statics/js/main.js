5c5d68d42c8545385c80b7b0571393834d9700b0
unknown <tolove0403@gmail.com>
2023-09-27 14:34:02 +0700

    fix code

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,4 +1,4 @@
 alert("Trang web vẫn đang trong quá trình build. Xin hãy quay trở lại sau");
-const header = document.getElementById("h1-color");
+const header = document.getElementById("demo");
 
-header.innerHTML("Chào mừng các bạn");
+header.innerHTML = "Chào mừng các bạn";
