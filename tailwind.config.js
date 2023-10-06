2434ba66fc7b209405406f6c46e3af645589d3db
takei_san204 <vutanh507@gmail.com>
2023-10-06 20:56:15 +0700

    Add file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/statics/js/main.js",
+    "/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
